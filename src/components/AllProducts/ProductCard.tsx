d4a78268f36a65d7a698e9a49084437d93ab717f
Paulo Victor <paulovictor.devs@gmail.com>
2024-01-14 23:59:50 -0300

    add shop styles

--- src/components/AllProducts/ProductCard.tsx
+++ src/components/AllProducts/ProductCard.tsx
@@ -11,8 +11,8 @@ export const ProductCard = ({ product }: ProductCard) => {
     <div className="product-card">
       <img src={product.image} alt="" />
       <div className="product-card-infos">
-        <span>{product.title}</span>
-        <div>
+        <div className="product-card-infos-title">{product.title}</div>
+        <div className="product-card-infos-bottom">
           <span>{product.category}</span>
           <span>{product.price}</span>
         </div>

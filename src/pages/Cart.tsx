c48fc63ccbed8c090b43f31768555c87c4ea3b3e
Paulo Victor <paulovictor.devs@gmail.com>
2024-01-17 00:38:28 -0300

    shopping cart refactoring

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import './styles/Cart.scss';
+import { CartItem } from '../components/ShoppingCart/CartItem';
 import ProductsList from '../components/ProductsList/ProductsList';
-import ShoppingCart from '../components/ShoppingCart/ShoppingCart';
 
 export const Cart = ({ items, setItems, totalItems }: ProductsList) => {
+  //
+
+  const totalCost = items.reduce(
+    (acc, curr) => acc + curr.quantity * curr.price,
+    0
+  );
+
   return (
-    <ShoppingCart items={items} setItems={setItems} totalItems={totalItems} />
+    <div className="shopping-cart">
+      <button className="return-button">← Continue Shopping</button>
+      <div className="shopping-cart-top">
+        <h1>Shopping Cart</h1>
+        <span
+          className="cart-items-counter"
+          data-testid="mock-cart-item-counter"
+        >
+          {totalItems === 1 ? `${totalItems} Item` : `${totalItems} Items`}
+        </span>
+      </div>
+
+      <div className="horizontal-line"></div>
+
+      <table>
+        <thead>
+          <tr className="cart-item-header-row">
+            <th scope="col">PRODUCT DETAILS</th>
+            <th scope="col">QUANTITY</th>
+            <th scope="col">PRICE</th>
+            <th scope="col">TOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => {
+            return (
+              <CartItem
+                items={items}
+                setItems={setItems}
+                currentProductId={item.id}
+                key={item.id}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="horizontal-line"></div>
+
+      <div className="order-summary">
+        <h1>Order Summary</h1>
+        <span
+          className="total-order-cost"
+          data-testid={'mock-cart-total-price'}
+        >
+          TOTAL COST: {totalCost}
+        </span>
+
+        <button className="cta-button">Checkout</button>
+      </div>
+    </div>
   );
 };
